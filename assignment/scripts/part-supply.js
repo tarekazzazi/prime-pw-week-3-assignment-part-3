0f224e5e4181aa6caff131f07b84a06ba5977174
Tarek Azzazi <azzazi.tarek@gmail.com>
2022-04-15 14:11:10 -0500

    Removed item

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -18,7 +18,8 @@ console.log(supplyChanges[supplyChanges.length - 5]);
 // 4. The last item was added by mistake. Remove it from the 'supplyChanges' 
 //    array & console.log the value removed.
 console.log('4. Removed item:');
-
+console.log(supplyChanges.pop());
+supplyChanges.pop();
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
 console.log('5. Adding 25 to supplyChanges.');
 
