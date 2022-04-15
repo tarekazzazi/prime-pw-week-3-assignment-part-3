ee1fe77f9e94b6684341ef4174a5698a6ec5fef5
Tarek Azzazi <azzazi.tarek@gmail.com>
2022-04-15 17:55:04 -0500

    Showing supplyChanges with 'For of'loop Answered

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -46,7 +46,9 @@ for (let i = 0; i < supplyChanges.length; i++) {
 console.log('---  Stretch Goals  ---');
 // 7. Rewrite the `for` loop from #6 as a `for of` loop. 
 console.log('7. Showing supplyChanges with "for of" loop');
-
+for( supply of supplyChanges){
+    console.log('for of loop',supply);
+} 
 // 8. Write a loop to determine the total number of parts available by
 //    adding up all the numbers in the 'supplyChanges' array.
 console.log('8. Total supplies available is:');
